c8917f809c952c4f5690a9cd11fd042e75736dd9
GuiLeoni14 <guileone167@gmail.com>
2022-02-24 21:54:25 -0300

    ajustando retorno de usuario

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -65,8 +65,7 @@ router.post('/login', async (req, res) => {
             },
             secret,
         );
-        user = await User.findById(user._id, '-password'); // 
-        created by me
+        user = await User.findById(user._id, '-password'); // created by me
         res.status(200).json({ message: 'Autenticação realizada com sucesso', token, user });
     } catch (error) {
         return res.status(500).json({ error: 'Error inesperado no servidor' });
